a7a5ce47385bda479012c2a515a9d157971da156
DMVidhuna06 <vidhunadm06@gmail.com>
2025-02-13 22:44:08 +0530

    Add files via upload

---
+++ simplesum.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+class Simplesum{
+public static void main(String[] args){
+scanner sc= new scanner(System.in);
+System.out.println("enter a:");
+int a= sc.nextInt();
+System.out.println("enter b:");
+int b= sc.nextInt();
+System.out.println("enter c:");
+int c= sc.nextInt();
+int d = a+b+c;
+system.out.println("addition of three numbers:"+d);
+}
+}
